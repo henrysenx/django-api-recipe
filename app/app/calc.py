6b67a8627fa3ef50988446e014111465a31863d3
e4ewebdev <webdev@e4email.net>
2023-03-25 12:58:39 +0100

    tutorial 8:4

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""""
+Calculator function
+"""
+
+def add(x,y):
+    """Add x and Y and return result"""
+    return x + y
+
+def subtract(x,y):
+    """suntract x from Y and return result"""
+    return y - x
