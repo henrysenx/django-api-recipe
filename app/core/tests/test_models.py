481c7fb50ce1c4943731f81dca335bf2e60a20e4
e4ewebdev <webdev@e4email.net>
2023-03-29 07:55:35 +0100

    Tutorial chapter 9 completed - lint 2

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,9 +3,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test Models"""
-
 
     def test_create_user_with_email_successful(self):
         """Test creating user with an email"""
